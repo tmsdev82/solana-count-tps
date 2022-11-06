822f40427880f92ada69c8e9d29fbf95a8df497f
Tim Steeman <tmdev82@gmail.com>
2022-11-06 15:58:24 +0100

    add function for retrieiving a block data object

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 use dotenv::dotenv;
-use solana_client::rpc_client::RpcClient;
+use solana_client::{rpc_client::RpcClient, rpc_config::RpcBlockConfig};
+use solana_transaction_status::{EncodedConfirmedBlock, UiTransactionEncoding};
+
+fn get_block(client: &RpcClient, block_num: u64) -> EncodedConfirmedBlock {
+    log::debug!("Getting block number: {}", block_num);
+
+    let config = RpcBlockConfig {
+        encoding: Some(UiTransactionEncoding::Base64),
+        max_supported_transaction_version: Some(0),
+        ..Default::default()
+    };
+
+    let block = client.get_block_with_config(block_num, config);
+    let encoded_block: EncodedConfirmedBlock = block.unwrap().into();
+
+    encoded_block
+}
 
 fn main() {
     dotenv().ok();
