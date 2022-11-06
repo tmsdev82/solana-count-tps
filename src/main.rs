8711954fd017818a7a7889b26293a2eb6ba457a7
Tim Steeman <tmdev82@gmail.com>
2022-11-06 14:01:19 +0100

    simple solana block chain connection app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use dotenv::dotenv;
+use solana_client::rpc_client::RpcClient;
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+    env_logger::init();
+
+    log::info!("Solana count transactions per second!");
+
+    let client = RpcClient::new("https://solana-api.projectserum.com");
+
+    let solana_version = client.get_version().unwrap().solana_core;
+    log::info!("Solana version: {}", &solana_version);
 }
